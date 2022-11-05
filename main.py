2b204a6b6f4d9a1acd480cbcf7243fe21eb72b50
Jose Mercado <josuemercado32@gmail.com>
2022-11-05 11:46:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import random
+pagerandom = random.choice(range(1, 41))
+idrandom = random.choice(range(5, 15))
+print(pagerandom)
+print(idrandom)
+url = "https://api.disneyapi.dev/characters?page={}".format(pagerandom)
+r = requests.get(url)
+j = r.json()
+r = idrandom
+name = j["data"][r]['name']
+img = j["data"][r]['imageUrl']
+films = j["data"][r]['films']
+shortFilms = j["data"][r]['shortFilms']
+tvShows = j["data"][r]['tvShows']
+videoGames = j["data"][r]['videoGames']
+parkAttractions = j["data"][r]['parkAttractions']
+print(f'Nombre:  {name}\nFilms:  {films}\nShortFilms:  {shortFilms}\nTvShows:  {tvShows}\nVideoGames:  {videoGames}\nParkAttractions:  {parkAttractions}\n')
+
